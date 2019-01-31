81a86606c8646be066bc30883fde91951f0d9857
theycyberboss <colbathnathan@gmail.com>
2019-01-31 09:07:19 -0500

    Bug Fix

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main{
   public static void main(String[] args){
 
     int[] list = {1,5,4,2,6};
-
+    
     Utils.Sorts.toSelectionSort(list);
     Utils.println(Utils.getArrayString(list));
 
